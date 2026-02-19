9330f744eb5a0a68bfdda3b86aaa339bb1f17ac0
sinchana s k <sinchusinchanask4@gmail.com>
2026-02-19 21:35:05 +0530

    feat(day-04): division and loop problem reslove

---
+++ Day-04/division.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    i=a//b
+    j=a/b
+    print(i)
+    print(j)
+    
+#day 04 - thursday
+
+#Task
+#The provided code stub reads two integers,  and , from STDIN.
+
+#Add logic to print two lines. The first line should contain the result of integer division,  
+# a//b . The second line should contain the result of float division,  a/b .
