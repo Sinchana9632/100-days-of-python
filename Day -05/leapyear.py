aaee960654fc24c38d7a922515e4637cfa2a2655
sinchana s k <sinchusinchanask4@gmail.com>
2026-02-20 22:02:01 +0530

    feat(day-05): find out thr leap year

---
+++ Day -05/leapyear.py
@@ -0,0 +1,38 @@
+"""
+In the Gregorian calendar, three conditions are used to identify leap years:
+
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+
+date 20 -02-2026
+"""
+
+
+
+
+
+
+
+
+
+
+
+def is_leap(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+
+
+
+
+
+year = int(input())
+print(is_leap(year))
